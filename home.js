19280b2a5cd0cf55f25c7d6b4f7afbaa490e5cc7
Thayna Corbicelis <tcorbicelis@gmail.com>
2026-03-13 11:43:35 -0300

    atualização

---
+++ home.js
@@ -0,0 +1,17 @@
+const text = "ReDedev | Rede de Desenvolvimento";
+let index = 0;
+
+function typeEffect(){
+
+if(index < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(index);
+index++;
+
+setTimeout(typeEffect,80);
+
+}
+
+}
+
+typeEffect();
